a5c42c68e71b9fadeb18f47a67ba7ba8bfcade9d
Borja Montseny <borja.montseny@gmail.com>
2023-11-02 21:59:36 +0100

    Commit

--- js/penjat.js
+++ js/penjat.js
@@ -1,4 +1,4 @@
-
+// Borja Montseny Peris DAW2 Dual Tarda
 /* Estructura "constant" del joc */
 var gameConfig = {
     liveLook: ["monster5.png", "monster4.png", "monster3.png", "monster2.png", "monster1.png", "monster0.png"],
@@ -28,8 +28,11 @@ var msgGameFail; // joc finalitzat sense vides
 var btnOk; // botó Continuar
 var livesText; // text del recuadre de vides
 var pressedKey; // tecla premuda
-var newGameButton;
+var newGameButton; // botó NEW GAME -> Partida nova (refrescar)
 var clueBox;
+var letters;
+var clueText;
+var clueLetter = "";
 
 window.onload = function () {
 
@@ -42,14 +45,14 @@ window.onload = function () {
     clueBox = document.getElementById("clue");
     livesText = document.getElementsByClassName("lives")[0];
     document.body.addEventListener("keydown", pressKey);
-
-    devConsoleInfo();
-    showInfoMessage();
     newGameButton.addEventListener("click", restartGame);
     btnOk.addEventListener("click", closeMessage);
     clueBox.addEventListener("mouseenter", giveClue);
+    letters = document.getElementById("letters");
+    clueText = document.getElementById("clue").getElementsByTagName("span")[0];
 
-
+    devConsoleInfo();
+    showWelcomeMessage();
 };
 
 // per ajudar a escollir una paraula dins l'array
@@ -59,12 +62,7 @@ function getRandomNumber(min, max) {
 
 function showInfoMessage() {
 
-    if (gameStatus.status === "playing") {
-        divInfo.style.display = "block";
-        msgWelcome.style.display = "block";
-        btnOk.style.display = "block";
-    }
-    else if (gameStatus.lives >= 1 && gameStatus.status === "completed") {
+    if (gameStatus.lives >= 1 && gameStatus.status === "completed") {
         divInfo.style.display = "block";
         msgGameSuccess.style.display = "block";
         btnOk.style.display = "block";
@@ -88,23 +86,41 @@ function closeMessage() {
 
 // funcio de perdre una vida, si es < 5, perdem
 function loseLive() {
-
     gameStatus.lives--;
 
     if (gameStatus.lives < 1) {
         gameStatus.status = "completed";
         msgGameFail.style.display = "block";
         btnOk.style.display = "block";
+
+        // eliminem el listener del botó continuar
+        btnOk.removeEventListener("click", closeMessage);
+
+        // Per a que canvii a un que reinicia la página
+        btnOk.addEventListener("click", restartGame);
     }
 
     livesText.innerHTML = (gameStatus.lives + "\n LIVES \nLEFT");
+
+    if (gameStatus.lives < 5 && gameStatus.lives >= 0) {
+        document.getElementById("monster").src = ("img/monster" + gameStatus.lives + ".png");
+    }
 }
 
 function giveClue() {
+
     // restem una vida
     loseLive();
-    // ensenyem una lletra no registrada ja
 
+    // ensenyem una lletra no registrada ja:
+    do { // calculem una lletra de la paraula que no hagi sigut mostrada encara
+        clueLetter = gameStatus.wordToGuess.charAt(getRandomNumber(0, gameStatus.wordToGuess.length));
+    } while (gameStatus.wordCompleted.includes(clueLetter));
+
+    clueText.innerHTML = clueLetter;
+
+    devConsoleInfo();
+    showInfoMessage();
 
 }
 
@@ -113,7 +129,7 @@ function pressKey(event) {
     pressedKey = event.key.toUpperCase();
 
     // if per admetre només lletres del teclat, descartem F4's..., Controls i Alts
-    if (!isNaN(pressedKey) || pressedKey.startsWith("F") || pressedKey.startsWith("C") || pressedKey.startsWith("A")) {
+    if (!/^[A-Z]$/i.test(pressedKey)) {
         return;
     }
 
@@ -121,20 +137,44 @@ function pressKey(event) {
     if (gameStatus.wordToGuess.includes(pressedKey)) {
         for (let i = 0; i < gameStatus.wordToGuess.length; i++) {
             if (gameStatus.wordToGuess[i] === pressedKey) {
-                gameStatus.wordCompleted = gameStatus.wordCompleted.substr(0, i) + pressedKey + gameStatus.wordCompleted.substr(i + 1);
+                gameStatus.wordCompleted = gameStatus.wordCompleted.substring(0, i) + pressedKey + gameStatus.wordCompleted.substring(i + 1);
+                letters.innerHTML = gameStatus.wordCompleted;
             }
         }
     } else if (!(gameStatus.wordToGuess.includes(pressedKey))) { // si no está la lletra a la paraula perdem una vida
         loseLive();
     }
+
+    // tant com si no tenim vides com si hem completat wordCompleted, status = completed
+    if (gameStatus.lives === 0) {
+        gameStatus.status = "completed";
+        showInfoMessage();
+    } else if (gameStatus.wordToGuess === gameStatus.wordCompleted) {
+        gameStatus.status = "completed";
+        showInfoMessage();
+    }
+
+    devConsoleInfo();
+
 }
+
 
 // missatges per al desenvolupador
 function devConsoleInfo() {
-    console.log("\n Status: " + gameStatus.status + "\n Lives: " + gameStatus.lives + "\n wordToGuess: " + gameStatus.wordToGuess + "\n wordCompleted: " + gameStatus.wordCompleted);
+    console.log("\n Status: " + gameStatus.status +
+        "\n Lives: " + gameStatus.lives +
+        "\n wordToGuess: " + gameStatus.wordToGuess +
+        "\n wordCompleted: " + gameStatus.wordCompleted +
+        "\n clueLetter: " + clueLetter);
 }
 
 // funció que reinicia el joc, li asignem a continuar si hem acabat
 function restartGame() {
     window.location.reload();
-} 
+}
+
+function showWelcomeMessage() {
+    divInfo.style.display = "block";
+    msgWelcome.style.display = "block";
+    btnOk.style.display = "block";
+}
